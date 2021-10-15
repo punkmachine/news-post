40c40111aea0fd81c06f0d3cbf3e5dbc98375c96
punkmachine <alex_rassydihin@mail.ru>
2021-10-15 14:24:48 +0600

    Добавляет appReducer.

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { likesReducer } from "./reducers/likesReducer";
 import { inputReducer } from "./reducers/inputReducer";
 import { commentsReducer } from "./reducers/commentsReducer";
+import { appReducer } from "./reducers/appReducer";
 
 export const rootReducer = combineReducers({
 	likesReducer,
 	inputReducer,
-	commentsReducer
+	commentsReducer,
+	appReducer
 });
