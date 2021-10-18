f6e814f604924351dfe83105c40e53775b5becb0
punkmachine <alex_rassydihin@mail.ru>
2021-10-18 14:32:07 +0600

    Добавляет тип для загрузчика новых комментов.

--- src/redux/types.js
+++ src/redux/types.js
@@ -7,6 +7,7 @@ export const COMMENT_CREATE = 'COMMENT_CREATE';
 export const COMMENT_UPD = 'UPD';
 export const COMMENT_DEL = 'DEL';
 export const COMMENTS_LOAD = 'COMMENTS_LOAD';
+export const LOAD_MORE_COMMENTS = 'LOAD_MORE_COMMENTS';
 
 export const LOADER_DISPLAY_ON = 'LOADER_DISPLAY_ON';
 export const LOADER_DISPLAY_OFF = 'LOADER_DISPLAY_OFF';
